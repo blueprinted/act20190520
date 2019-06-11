6242c4f2d7603fa1dc4bacfaac4903cdc472804b
endloop <blueprinted@qq.com>
2019-06-12 03:43:49 +0800

    更新

--- bend/doc/tables.sql
+++ bend/doc/tables.sql
@@ -42,7 +42,8 @@ CREATE TABLE `act20190520_user` (
   UNIQUE KEY `phone_number` (`phone_number`),
   KEY `nickname` (`nickname`),
   KEY `ctime` (`ctime`,`mtime`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+INSERT INTO `act20190520_user` VALUES (1,'李逍遥',26,1,'',182,'神仙',110000,110100,110101,110000,110100,110101,'天宫大学','0','13800000001','lixiaoyao',10,4,'upload/uploadImages/huge.jpeg','A','A','A','A','0','0',110000,110100,110101,'0','0',0,'0.0.0.0',1560274308,0),(2,'白子画',28,1,'',178,'神仙',110000,110100,110101,110000,110100,110101,'天宫大学','0','13800000002','baizihua',10,4,'upload/uploadImages/huojianhua.jpeg','A','A','A','A','0','0',110000,110100,110101,'0','0',0,'0.0.0.0',1560274308,0),(3,'明诚',27,1,'',180,'凡人',110000,110100,110101,110000,110100,110101,'地宫大学','0','13800000003','mingcheng',10,4,'upload/uploadImages/huojianhua.jpeg','A','A','A','A','0','0',110000,110100,110101,'0','0',0,'0.0.0.0',1560274308,0),(4,'赵灵儿',18,2,'',172,'神仙',110000,110100,110101,110000,110100,110101,'天宫大学','0','13800000004','zhaolinger',10,4,'upload/uploadImages/liuyifei.jpeg','A','A','A','A','0','0',110000,110100,110101,'0','0',0,'0.0.0.0',1560274308,0),(5,'花千骨',18,2,'',168,'神仙',110000,110100,110101,110000,110100,110101,'天宫大学','0','13800000005','huaqiangu',10,4,'upload/uploadImages/zhaoliying.jpeg','A','A','A','A','0','0',110000,110100,110101,'0','0',0,'0.0.0.0',1560274308,0),(6,'陆雪琪',18,2,'',168,'神仙',110000,110100,110101,110000,110100,110101,'天宫大学','0','13800000006','luxueqi',10,4,'upload/uploadImages/yangzi.jpeg','A','A','A','A','0','0',110000,110100,110101,'0','0',0,'0.0.0.0',1560274308,0),(7,'tuster',26,1,'',168,'工人',110000,110100,110105,110000,110100,110105,'北京','4','13811280234','weixin',2,2,'upload/uploadImages/201906/12/201906120159189367Cb2p49Z.png','B','B','A','B','5','6',110000,110100,110105,'5','4',0,'::1',1560275958,1560280665);
 
 ---
 --- 用户匹配表
@@ -51,7 +52,7 @@ CREATE TABLE `act20190520_user_match` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `master_uid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '主uid',
   `match_uid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '匹配到的uid',
-  `liked` tinyint(1)  NOT NULL DEFAULT '0' COMMENT '是否点赞',
+  `liked` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否点赞',
   `ctime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '匹配时间',
   `mtime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`),
